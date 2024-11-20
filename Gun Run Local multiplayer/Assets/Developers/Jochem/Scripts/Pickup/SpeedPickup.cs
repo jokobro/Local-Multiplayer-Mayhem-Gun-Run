e22beb53b74cf1d88a64a5a6d53290b69d3f7ae8
jokobro <jochemstarwarsclonewars@gmail.com>
2024-11-20 09:42:01 +0100

    playermovement, pickups

---
+++ Gun Run Local multiplayer/Assets/Developers/Jochem/Scripts/Pickup/SpeedPickup.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpeedPickup : MonoBehaviour
+{
+    private MeshRenderer _meshRenderer;
+    private PlayerController _player;
+
+    private void Start()
+    {
+        activateSpeedPickup();
+    }
+
+   /* private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "player")
+        {
+            FindObjectOfType<PlayerController>().RunSpeed = 4.5f;
+            StartCoroutine(resetRunSpeedBack());
+        }
+    }*/
+
+
+    private void activateSpeedPickup()
+    {
+        FindObjectOfType<PlayerController>().RunSpeed = 4.5f;
+        StartCoroutine(resetRunSpeedBack());
+    }
+
+    private IEnumerator resetRunSpeedBack()
+    {
+        yield return new WaitForSeconds(12f);
+        FindObjectOfType<PlayerController>().RunSpeed = 3f;
+        Debug.Log("Runspeed is terug naar normaal");
+        Destroy(gameObject);
+    }
+}
