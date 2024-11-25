7eb9f3c6796d7f1f03c1d4e0e8bc140ca23902f8
DMClone <230381@student.glu.nl>
2024-11-25 11:08:58 +0100

    Projectile trap volley logic and coroutines

--- Gun Run Local multiplayer/Assets/Developers/Dani/Scripts/ProjectileTrap.cs
+++ Gun Run Local multiplayer/Assets/Developers/Dani/Scripts/ProjectileTrap.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 
 public class ProjectileTrap : Trap
 {
+    [SerializeField] private GameObject projectile;
+
+    [SerializeField] private int projectilesPerVolley;
+    [SerializeField] private int volleyAmount;
+    [SerializeField] private float timeBetweenProjectiles;
+    [SerializeField] private float timeBetweenVolleys;
+    private int onProjectile;
+    private int onVolley;
+
     public override void ActivateTrap()
     {
-    
+        StartCoroutine(Volley());
+    }
+
+    IEnumerator Volley()
+    {
+        Instantiate(projectile, transform.position + (transform.forward * 1), Quaternion.identity);
+        onProjectile++;
+        yield return new WaitForSeconds(timeBetweenProjectiles);
+        if (onProjectile >= projectilesPerVolley)
+        {
+            StartCoroutine(WaitForVolley());
+        }
+        else
+        {
+            StartCoroutine(Volley());
+        }
+    }
+
+    IEnumerator WaitForVolley()
+    {
+        yield return new WaitForSeconds(timeBetweenVolleys);
+        StartCoroutine(Volley());
     }
 }
