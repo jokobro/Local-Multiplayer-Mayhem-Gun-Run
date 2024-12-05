c01826b8b5d058c93eddc3f37d93cf342f415db8
jokobro <jochemstarwarsclonewars@gmail.com>
2024-12-05 12:33:29 +0100

    Lobby needs fixing

--- Gun Run Local multiplayer/Assets/Developers/Dani/Scripts/Finish.cs
+++ Gun Run Local multiplayer/Assets/Developers/Dani/Scripts/Finish.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+   [SerializeField] private GameObject _winScreenRunners;
+
     private void OnTriggerEnter(Collider other)
     {
         if ((other.gameObject.GetComponent<PlayerState>() != null) && other.gameObject.GetComponent<PlayerState>().state == PlayerStates.alive)
         {
             Debug.Log("Runners won");
+            _winScreenRunners.SetActive(true);
+            
+            //nog implementeren welke player won
         }
     }
 }
