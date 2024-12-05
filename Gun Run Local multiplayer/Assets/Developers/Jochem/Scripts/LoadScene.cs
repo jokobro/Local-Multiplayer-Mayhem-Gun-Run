d1f99e59b562e948d23bc48f540b00963f68fc71
jokobro <jochemstarwarsclonewars@gmail.com>
2024-12-05 10:47:52 +0100

    UI added

--- Gun Run Local multiplayer/Assets/Developers/Jochem/Scripts/LoadScene.cs
+++ Gun Run Local multiplayer/Assets/Developers/Jochem/Scripts/LoadScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LoadScene : MonoBehaviour
-{
+{    [SerializeField] private GameObject _creditsPanel;
     public bool StartButtonPressed;
 
     public void LoadMainMenu()
@@ -20,6 +20,31 @@ public class LoadScene : MonoBehaviour
     public void ReloadScene()
     {
        string currentSceneName = SceneManager.GetActiveScene().name;
-        SceneManager.LoadScene(currentSceneName);
+       SceneManager.LoadScene(currentSceneName);
+       Debug.Log("reload");
     }
+
+    public void LoadLobby()
+    {
+      SceneManager.LoadScene("");
+      Debug.Log("Load lobby");
+    }
+
+    public void LoadCredits()
+    {
+     _creditsPanel.SetActive(true);
+    }
+
+    public void returnMainMenuCredits()
+    {
+      _creditsPanel.SetActive(false);
+    }
+     
+    public void Quit()
+    {
+      Debug.Log("Has Quit");
+      Application.Quit();
+    }
+
+
 }
